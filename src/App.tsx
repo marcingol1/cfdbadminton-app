351fe80a0d812de0a2db5a0aad975ad74f5a5848
Marcin Gol <golmarcin@gmail.com>
2018-11-18 21:26:59 +0100

    Upgrade React for working Hooks API from 16.7 version

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [ count, setCount ] = useState(120);
+
+  return (
+    <div className="counter">
+      <p>You clicked {count} times!</p>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+    </div>
+  )
 }
 
 export default App;
