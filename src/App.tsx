c6343296bcce2f7243cdcaa0fd2a6094a13100d1
Marcin Gol <golmarcin@gmail.com>
2018-11-28 21:09:27 +0100

    add apollo to front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
 
+import Client, { AUTH_TYPE } from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from 'aws-appsync-react';
+import AppSync from './AppSync';
+
+const client = new Client({
+  url: AppSync.aws_appsync_graphqlEndpoint,
+  region: AppSync.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.API_KEY,
+    apiKey: AppSync.aws_appsync_apiKey,
+  },
+});
+
 const App = () => {
-  const [ count, setCount ] = useState(120);
+  const [count, setCount] = useState(120);
 
   return (
-    <div className="counter">
-      <p>You clicked {count} times!</p>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-    </div>
-  )
-}
+    <ApolloProvider client={client}>
+      <Rehydrated>
+        <p>You clicked {count} times!</p>
+        <button onClick={() => setCount(count + 1)}>Click me!</button>
+      </Rehydrated>
+    </ApolloProvider>
+  );
+};
 
 export default App;
