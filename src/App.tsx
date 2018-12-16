9935a8fc808aa89e1d73aa075151e00947934c89
Marcin Gol <golmarcin@gmail.com>
2018-12-16 10:47:30 +0100

    working amplify authentication!

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import React, { useState } from 'react';
-import Client, { AUTH_TYPE } from 'aws-appsync';
-import { ApolloProvider } from 'react-apollo';
-import { Rehydrated } from 'aws-appsync-react';
+import Amplify from 'aws-amplify';
+import React, { Fragment, useState } from 'react';
+import aws_exports from './aws-exports.js';
+import { withAuthenticator } from 'aws-amplify-react';
 
-import AppSync from './AppSync';
-import ListPlayers from './ListPlayers';
-
-const client = new Client({
-  url: AppSync.aws_appsync_graphqlEndpoint,
-  region: AppSync.aws_appsync_region,
-  auth: {
-    type: AUTH_TYPE.API_KEY,
-    apiKey: AppSync.aws_appsync_apiKey,
-  },
-});
+Amplify.configure(aws_exports);
 
 const App = () => {
   const [count, setCount] = useState(120);
 
   return (
-    <ApolloProvider client={client}>
-      <Rehydrated>
-        <ListPlayers />
-        <p>You clicked {count} times!</p>
-        <button onClick={() => setCount(count + 1)}>Click me!</button>
-      </Rehydrated>
-    </ApolloProvider>
+    <Fragment>
+      <p>You clicked {count} times!</p>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+    </Fragment>
   );
 };
 
-export default App;
+export default withAuthenticator(App, true);
