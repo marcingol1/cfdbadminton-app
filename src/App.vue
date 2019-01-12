85b32da0405a5b3e5ec282cd30e47176c4884a4e
Marcin Gol <golmarcin@gmail.com>
2019-01-12 16:21:49 +0100

    basic working data usage of object Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <amplify-authenticator></amplify-authenticator>
+    <button v-if="signedIn" @click="logout">Log out</button>
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
@@ -8,6 +10,45 @@
     <router-view/>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { components } from "aws-amplify-vue";
+import { Auth } from "aws-amplify";
+import { AmplifyEventBus } from "aws-amplify-vue";
+
+export default Vue.extend({
+  components: {
+    ...components
+  },
+  data() {
+    return { signedIn: false };
+  },
+  async beforeCreate() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      this.signedIn = true;
+    } catch (err) {
+      this.signedIn = false;
+    }
+  },
+  methods: {
+    async logout() {
+      Auth.signOut()
+        .then(test => {
+          console.log(test, this.$data, "test");
+          this.signedIn = true;
+        })
+        .catch(() => {
+          this.signedIn = false;
+        });
+      this.signedIn = !this.signedIn;
+    }
+  }
+});
+</script>
+
 
 <style lang="scss">
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
-    <button @click="logout">Log out</button>
     <div class="home">
       <img alt="Vue logo" src="../assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
@@ -12,36 +10,10 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import { components } from "aws-amplify-vue";
-import { AmplifyEventBus } from "aws-amplify-vue";
-
-import { Auth } from "aws-amplify";
 
 @Component({
   components: {
-    HelloWorld,
-    components
-  },
-  computed: {
-    authConfig() {
-      console.log(this.$Amplify);
-      return {};
-    }
-  },
-  methods: {
-    async logout() {
-      console.log("logging out!");
-      const logoutStatus = await await Auth.signOut();
-      console.log("result: ", logoutStatus);
-      return logoutStatus;
-    }
-  },
-  created: function() {
-    AmplifyEventBus.$on("authState", (info: any) => {
-      console.log(
-        `Here is the auth event that was just emitted by an Amplify component: ${info}`
-      );
-    });
+    HelloWorld
   }
 })
 export default class Home extends Vue {}
