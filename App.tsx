12a4ffaf3533e5aa7d8af302741da15332ba7ae9
Marcin Gol <golmarcin@gmail.com>
2020-02-08 13:52:35 +0100

    build cfdbadminton app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={{
+          color: 'papayawhip',
+          backgroundColor: 'black',
+          fontSize: 20,
+          padding: 20,
+          height: '100%'
+        }}
+      >
+        Open up App.tsx to start working on your app!!!
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff'
   }
 });
